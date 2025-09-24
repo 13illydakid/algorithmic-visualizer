f5f2b475fd3f728e71d571eff476a058202de6c5
Billy Chiu <billychiu517@gmail.com>
2025-09-23 23:44:17 -0700

    default speed changed

--- src/reducers/speed/index.js
+++ src/reducers/speed/index.js
@@ -5,7 +5,7 @@ export const SET_SPEED = "SET_SPEED";
 export const setSpeed = createAction(SET_SPEED);
 
 // Default speed multiplier (5x)
-const initialState = 5;
+const initialState = 2;
 
 export const speed = handleActions({
   SET_SPEED: (state, { payload }) => payload,
